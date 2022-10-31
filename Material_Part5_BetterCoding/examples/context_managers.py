40fcde94e207d1fdeacf54ae1daff03721d28e54
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2022-10-31 11:38:31 +0100

    add examples for part 5 (#16)
    * more fixes - part 1 complete
    
    * more fixes - part 2
    
    * update part 3
    
    * update task description
    
    * part 4
    
    * more fixes
    
    * add examples for part 5
    
    * more examples
    
    * fix
    
    * add links

---
+++ Material_Part5_BetterCoding/examples/context_managers.py
@@ -0,0 +1,14 @@
+# original version
+
+f = open("data/efield.t","r")
+
+numbers = f.read()
+
+f.close()
+
+# use context manager instead
+
+with open("data/efield.t","r"):
+    numbers = f.read()
+
+# for example for threads, databases, etc - for improved resource handling
